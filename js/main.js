d52f91cc1a21c6cf5827fe1e92889f52a90019c1
h3xar0n <aron@glambyadelina.com>
2017-04-06 13:38:52 -0400

    Provided colors that color class can switch to

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 var main = function() {
 
   var colorClass = '';
-  
+
   $('select-color').on('click', 
     function () {
       var selectedColor = $(this).attr('class');
       switch (selectedColor) {
-        case 'select-color cyan not-selected':
-          // set color on box div
+        case 'select-color magenta not-selected':
+          colorClass = 'magenta';
         break;
         case 'select-color cyan not-selected':
-          // set color on box div
+          colorClass = 'cyan';
         break;
-        case 'select-color cyan not-selected':
-          // set color on box div
+        case 'select-color yellow not-selected':
+          colorClass = 'yellow';
         break;
       }
   });
