cf1ce8e2d7465ca4439bf41b6eb2d2a8f21c08f5
h3xar0n <aron@glambyadelina.com>
2017-04-06 13:51:30 -0400

    pegs light up, fixed dumb syntax error

--- js/main.js
+++ js/main.js
@@ -1,31 +1,31 @@
 var main = function() {
-
+  
   var colorClass = '';
-
-  $('select-color').on('click', 
-    function () {
+  
+  $('.select-color').on('click', 
+    function() {
       var selectedColor = $(this).attr('class');
-
+    
       switch (selectedColor) {
         case 'select-color magenta not-selected':
           colorClass = 'magenta';
         break;
-        case 'select-color cyan not-selected':
-          colorClass = 'cyan';
-        break;
         case 'select-color yellow not-selected':
           colorClass = 'yellow';
         break;
+        case 'select-color cyan not-selected':
+          colorClass = 'cyan';
+        break;
       }
-
+    
       $(this).removeClass('not-selected');
       $(this).siblings().addClass('not-selected');
   });
-
+  
   $('.box').on('click',
     function() {
-    
+      $(this).addClass(colorClass);
   });
-};
+}
 
-($document).ready(main);
+$(document).ready(main);
