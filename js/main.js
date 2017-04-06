8c4ae6b763f609a23486c074ac42602e965395c6
h3xar0n <aron@glambyadelina.com>
2017-04-06 13:33:58 -0400

    Switch statement for each color

--- js/main.js
+++ js/main.js
@@ -2,6 +2,17 @@ var main = function() {
   $('select-color').on('click', 
     function () {
       var selectedColor = $(this).attr('class');
+      switch (selectedColor) {
+        case 'select-color cyan not-selected':
+          // set color on box div
+        break;
+        case 'select-color cyan not-selected':
+          // set color on box div
+        break;
+        case 'select-color cyan not-selected':
+          // set color on box div
+        break;
+      }
   });
 };
 
